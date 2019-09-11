c764acc8336fd816d47dd7b0a55c47b12cbd2cc3
Daylight <andryha.p@gmail.com>
2019-09-11 13:26:16 +0300

    Updates

---
+++ precache-manifest.e00160f2a988fb1a14edd53fc68253e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7389c9521f181897b4d161ac207bab",
+    "url": "/Worktime-App/index.html"
+  },
+  {
+    "revision": "7af96173275ad0640a57",
+    "url": "/Worktime-App/static/css/main.d94bc4b0.chunk.css"
+  },
+  {
+    "revision": "b4d76ee26071bebf96db",
+    "url": "/Worktime-App/static/js/2.5bcb553c.chunk.js"
+  },
+  {
+    "revision": "7af96173275ad0640a57",
+    "url": "/Worktime-App/static/js/main.6ab18554.chunk.js"
+  },
+  {
+    "revision": "a1a106b951992813b8b3",
+    "url": "/Worktime-App/static/js/runtime~main.035ca209.js"
+  }
+]);
