8a09f68dcae90675e51c34a0684f9bfa154191d8
Daylight <andryha.p@gmail.com>
2019-09-19 20:18:47 +0300

    Updates

--- precache-manifest.93e06181ae8b457136dc97db881ac084.js
+++ precache-manifest.93e06181ae8b457136dc97db881ac084.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef7dafe90a8c113f8b2d4bcc537c0aa0",
+    "revision": "8cc424277b413a76f8240a5b57273b70",
     "url": "/Worktime-App/index.html"
   },
   {
-    "revision": "97006a3b7e3874c1ca65",
-    "url": "/Worktime-App/static/css/main.8255c669.chunk.css"
+    "revision": "673d9b8d35ac796ccdec",
+    "url": "/Worktime-App/static/css/main.eb28ee50.chunk.css"
   },
   {
     "revision": "ccac9c119d613838580d",
     "url": "/Worktime-App/static/js/2.a575e77b.chunk.js"
   },
   {
-    "revision": "97006a3b7e3874c1ca65",
-    "url": "/Worktime-App/static/js/main.127ef5a9.chunk.js"
+    "revision": "673d9b8d35ac796ccdec",
+    "url": "/Worktime-App/static/js/main.6f9ea33a.chunk.js"
   },
   {
     "revision": "a1a106b951992813b8b3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Worktime-App/precache-manifest.60e66d9ed4963f04cafe0433f7e2b03c.js"
+  "/Worktime-App/precache-manifest.93e06181ae8b457136dc97db881ac084.js"
 );
 
 self.addEventListener('message', (event) => {
